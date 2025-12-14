7b6887fb0c9f10892d470e126343bddb54a66462
FloKnapp <office@florianknapp.de>
2025-12-14 22:05:29 +0100

    NIX-1 More tests - Added session start again

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,8 @@ declare(strict_types=1);
 
 use NixPHP\Session\Core\Session;
 use function NixPHP\app;
+use function NixPHP\Session\session;
 
 app()->container()->set(Session::class, fn () => new Session());
+
+session()->start();
